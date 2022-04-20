f242193b487574099bfc565e3a64fe992cc36692
Scott <sfitzpatrick55@gmail.com>
2022-04-19 20:53:21 -0400

    Populate comment class

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,11 @@ namespace SocialMedia.Models
 {
     public class Comment
     {
+        [ForeignKey("Post")]
+        [ForeignKey("User")]
         public int Id { get; set; }
         [Required]
         public string Text { get; set; }
-        [Required]
-        public Guid AuthorId { get; set; }
-        [Required]
-        
+        [Required]        
     }
 }

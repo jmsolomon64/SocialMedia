05899e72de62b9a8bd56f4dea40b8e79a256a710
cstazinski29 <cstazinski@gmail.com>
2022-04-20 20:48:43 -0400

    change namespaces to all reference SocialMedia instead of SocialMediaAPI, edited UserController to use ToListAsync() instead of ToListArray(), added debug file, installed packages for AspNetCore.Mvc and DependencyInjection

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;//needed to gain inheritance to the controller class
 
-namespace SocialMediaAPI.Controllers
+namespace SocialMedia.Controllers
 {
     [ApiController] //gives dotnet heads up that this controller is an API controller not MVC
     [Route("[controller]")] //lets dotnet know what url access this controller
